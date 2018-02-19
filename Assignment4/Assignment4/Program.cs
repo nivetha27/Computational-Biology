096d6462f5d5e2a7dae6ce81880bf0fbc4d1f1bb
nivetha27 <nivetha.sathyarajan@gmail.com>
2018-02-18 22:14:43 -0800

    refactoring

--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -34,24 +34,21 @@ namespace Assignment4
     public const char defaultCharReplaceNonACGT = 'T';
     public static string fileDirectory = @"C:\Users\nsathya\Documents\GitHub\Computational-Biology\Assignment4\Assignment4\";
     public static string sequences = "316664";
-    public static double[,] HMMParams;
+    public static double[,] transitionStates;
     public static Dictionary<char, double[]> emissionStates = new Dictionary<char, double[]>();
     public static int maxLenInRolls = Int32.MinValue;
     public static StateEstimation[][] HMM = new StateEstimation[numStates][];
     static void Main(string[] args)
     {
-      bool runTestData = false;
+      bool runTestData = true;
       if (runTestData) {
-        initializeTestTransitionAndEmissionStates(true);
+        initializeTestTransitionAndEmissionStates(false);
       } else {
         initializeTransitionAndEmissionStates();
         sequences = readAFastaFile(fileDirectory + "GCF_000091665.1_ASM9166v1_genomic.fna");
         // Console.WriteLine(sequences);
       }
-
-      initializeTransitionEmissionArray();
-
-      // trace back
+      computeHMMForward();
       traceBack();
       // print(HMM);
 
@@ -59,15 +56,7 @@ namespace Assignment4
       Console.ReadLine();
     }
 
-    public static char validateSequenceChar(char x){
-      char c = x;
-      if (!emissionStates.ContainsKey(c)) {
-        c = defaultCharReplaceNonACGT;
-      }
-      return c;
-    }
-
-    public static void initializeTransitionEmissionArray() {
+    public static void computeHMMForward() {
       for (int i = 0; i < numStates; i++)
         HMM[i] = new StateEstimation[sequences.Length];
 
@@ -76,13 +65,13 @@ namespace Assignment4
           char c = validateSequenceChar(sequences[col]);
           HMM[row][col] = new StateEstimation(numStates);
           if (col == 0) {
-            // HMM[row][col].value = (HMMParams[numStates, row]) * (emissionStates[c][row]);
-            HMM[row][col].value = Math.Log(HMMParams[numStates, row]) + Math.Log(emissionStates[c][row]);
+            // HMM[row][col].value = (transitionStates[numStates, row]) * (emissionStates[c][row]);
+            HMM[row][col].value = Math.Log(transitionStates[numStates, row]) + Math.Log(emissionStates[c][row]);
           } else {
             HMM[row][col].value = Double.MinValue;
             for (int i = 0; i < numStates; i++) {
-              // double stateValues = (HMM[i][col - 1].value) * (HMMParams[i, row]) * (emissionStates[c][row]);
-              double stateValues = (HMM[i][col - 1].value) + Math.Log(HMMParams[i, row]) + Math.Log(emissionStates[c][row]);
+              // double stateValues = (HMM[i][col - 1].value) * (transitionStates[i, row]) * (emissionStates[c][row]);
+              double stateValues = (HMM[i][col - 1].value) + Math.Log(transitionStates[i, row]) + Math.Log(emissionStates[c][row]);
               HMM[row][col].value = Math.Max(HMM[row][col].value, stateValues);
               HMM[row][col].allStates[i] = stateValues;
             }
@@ -140,6 +129,54 @@ namespace Assignment4
       if (shouldPrintViterbi)
         printViterbiPath(reverse(mostProbablePath.ToString()));
     }
+    public static void initializeTransitionAndEmissionStates() {
+      transitionStates = new double[numStates + 1, numStates] {
+      // {state 1, State 2}
+         {.9999, .0001}, // State 1
+         { 0.01,0.99}, // State 2
+         {.9999, .0001} // Begin 
+      };
+      emissionStates.Add('A', new double[numStates] { 0.25, 0.20 }); // {state 1, State 2}
+      emissionStates.Add('C', new double[numStates] { 0.25, 0.30 });
+      emissionStates.Add('G', new double[numStates] { 0.25, 0.30 });
+      emissionStates.Add('T', new double[numStates] { 0.25, 0.20 });
+    }
+
+    public static void initializeTestTransitionAndEmissionStates(bool toyExample = true) {
+      if (toyExample) {
+        sequences = "316664";
+        transitionStates = new double[numStates + 1, numStates] {
+           {0.6, 0.40},
+           {0.17, 0.83},
+           {0.52, 0.48} //begin
+        };
+      } else {
+        sequences = "315116246446644245311321631164152133625144543631656626566666651166453132651245636664631636663162326455236266666625151631222555441666566563564324364131513465146353411126414626253356366163666466232534413661661163252562462255265252266435353336233121625364414432335163243633665562466662632666612355245242";
+        transitionStates = new double[numStates + 1, numStates] {
+           {0.9, 0.10}, // {0.6, 0.40},
+           {0.05, 0.95}, //  {0.17, 0.83},
+           {0.52, 0.48} //begin
+        };
+      }
+      emissionStates.Add('1', new double[numStates] { 1.0 / 10, 1.0 / 6 }); // 0 = Loaded, 1 = Fair
+      emissionStates.Add('2', new double[numStates] { 1.0 / 10, 1.0 / 6 });
+      emissionStates.Add('3', new double[numStates] { 1.0 / 10, 1.0 / 6 });
+      emissionStates.Add('4', new double[numStates] { 1.0 / 10, 1.0 / 6 });
+      emissionStates.Add('5', new double[numStates] { 1.0 / 10, 1.0 / 6 });
+      emissionStates.Add('6', new double[numStates] { 1.0 / 2, 1.0 / 6 });
+    }
+
+    /*************************************************************************PRINT FUNCTIONS**********************************************************************************************************/
+    public static void print(StateEstimation[][] a) {      
+      for (int i = 0; i < a.Length; i++) {
+        StringBuilder s = new StringBuilder(addTrailiingWhiteSpaces(i.ToString(), 1));
+        for (int j =0; j < a[0].Length; j++) {
+          string rollProbStr = a[i][j].value.ToString();
+          s.Append(addTrailiingWhiteSpaces(rollProbStr, maxLenInRolls - rollProbStr.Length));
+        }
+        Console.WriteLine(s.ToString());
+      }
+    }
 
     public static void printViterbiPath(string viterbi) {
       // Console.WriteLine(String.Format("Viterbi path is: {0}", viterbi));
@@ -175,6 +212,15 @@ namespace Assignment4
       }
     }
 
+    /*************************************************************************HELPER FUNCTIONS********************************************************************************************************/
+    private static char validateSequenceChar(char x){
+      char c = x;
+      if (!emissionStates.ContainsKey(c)) {
+        c = defaultCharReplaceNonACGT;
+      }
+      return c;
+    }
+    
     private static string readAFastaFile(string fullFileName) {
       StringBuilder seq = new StringBuilder();
       bool firstSeqSeen = false;
@@ -197,55 +243,8 @@ namespace Assignment4
       }
       return seq.ToString();
     }
-    public static void initializeTransitionAndEmissionStates() {
-      HMMParams = new double[numStates + 1, numStates] {
-      // {state 1, State 2}
-         {.9999, .0001}, // State 1
-         { 0.01,0.99}, // State 2
-         {.9999, .0001} // Begin 
-      };
-      emissionStates.Add('A', new double[numStates] { 0.25, 0.20 }); // {state 1, State 2}
-      emissionStates.Add('C', new double[numStates] { 0.25, 0.30 });
-      emissionStates.Add('G', new double[numStates] { 0.25, 0.30 });
-      emissionStates.Add('T', new double[numStates] { 0.25, 0.20 });
-    }
-
-    public static void initializeTestTransitionAndEmissionStates(bool toyExample = true) {
-      if (toyExample) {
-        sequences = "316664";
-        HMMParams = new double[numStates + 1, numStates] {
-           {0.6, 0.40},
-           {0.17, 0.83},
-           {0.52, 0.48} //begin
-        };
-      } else {
-        sequences = "315116246446644245311321631164152133625144543631656626566666651166453132651245636664631636663162326455236266666625151631222555441666566563564324364131513465146353411126414626253356366163666466232534413661661163252562462255265252266435353336233121625364414432335163243633665562466662632666612355245242";
-        HMMParams = new double[numStates + 1, numStates] {
-           {0.9, 0.10}, // {0.6, 0.40},
-           {0.05, 0.95}, //  {0.17, 0.83},
-           {0.52, 0.48} //begin
-        };
-      }
-      emissionStates.Add('1', new double[numStates] { 1.0 / 10, 1.0 / 6 }); // 0 = Loaded, 1 = Fair
-      emissionStates.Add('2', new double[numStates] { 1.0 / 10, 1.0 / 6 });
-      emissionStates.Add('3', new double[numStates] { 1.0 / 10, 1.0 / 6 });
-      emissionStates.Add('4', new double[numStates] { 1.0 / 10, 1.0 / 6 });
-      emissionStates.Add('5', new double[numStates] { 1.0 / 10, 1.0 / 6 });
-      emissionStates.Add('6', new double[numStates] { 1.0 / 2, 1.0 / 6 });
-    }
-
-    public static void print(StateEstimation[][] a) {      
-      for (int i = 0; i < a.Length; i++) {
-        StringBuilder s = new StringBuilder(addTrailiingWhiteSpaces(i.ToString(), 1));
-        for (int j =0; j < a[0].Length; j++) {
-          string rollProbStr = a[i][j].value.ToString();
-          s.Append(addTrailiingWhiteSpaces(rollProbStr, maxLenInRolls - rollProbStr.Length));
-        }
-        Console.WriteLine(s.ToString());
-      }
-    }
-
-    public static string addTrailiingWhiteSpaces(string x, int c) {
+    
+    private static string addTrailiingWhiteSpaces(string x, int c) {
       StringBuilder s = new StringBuilder(x);
       for (int i = 1; i <=c; i++)
         s.Append(" ");
@@ -253,7 +252,7 @@ namespace Assignment4
        return s.ToString(); 
     }
 
-    public static string reverse(string s) {
+    private static string reverse(string s) {
       char[] x = s.ToCharArray();
       Array.Reverse(x);
       return new string(x);
